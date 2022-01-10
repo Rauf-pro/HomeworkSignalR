706f06963336de6c0d1832975911701b49e976ee
Rauf-pro <87373669+Rauf-pro@users.noreply.github.com>
2022-01-10 13:03:24 +0400

    Added HomeworkSignalR

---
+++ SignalRChat/SignalRChat/ViewModel/VmMessage.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.Web.CodeGeneration.Contracts.Messaging;
+using SignalRChat.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Message = SignalRChat.Models.Message;
+
+namespace SignalRChat.ViewModel
+{
+    public class VmMessage
+    {
+        public List<Message> Messages { get; set; }
+        public CustomUser User { get; set; }
+        public string SenderId { get; set; }
+    }
+}
